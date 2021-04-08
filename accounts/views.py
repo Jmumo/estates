d7335dace21675218a73802b4c3bbf416013d062
Jmumo <48655696+Jmumo@users.noreply.github.com>
2021-04-08 20:13:28 +0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages,auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 def login(request):
     if request.method =='POST':
